384b3fe1c56e0a02f752c18bacb479f0412f7e6f
brianjimm <117360679+brianjimm@users.noreply.github.com>
2024-10-13 13:20:38 +0300

    Update settings.py

--- registration/settings.py
+++ registration/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenvSECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
